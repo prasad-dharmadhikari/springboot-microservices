39d07862fb266013e4294043138dec509b310deb
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2023-08-24 16:25:30 +0530

    ADDED: RestTemaplate Bean configurations

---
+++ user-service/src/main/java/com/springboot/microservices/userservice/UserServiceConfiguration.java
@@ -0,0 +1,15 @@
+package com.springboot.microservices.userservice;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class UserServiceConfiguration {
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+}
