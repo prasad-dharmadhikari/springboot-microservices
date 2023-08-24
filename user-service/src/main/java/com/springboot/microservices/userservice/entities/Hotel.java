753ce4598d339a08e074be25215b1459e2fc0b2f
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2023-08-24 16:26:08 +0530

    ADDED: Hotel class to receive the hotel data

---
+++ user-service/src/main/java/com/springboot/microservices/userservice/entities/Hotel.java
@@ -0,0 +1,24 @@
+package com.springboot.microservices.userservice.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Hotel {
+
+	private String id;
+	
+	private String name;
+	
+	private String location;
+	
+	private String about;
+	
+}
