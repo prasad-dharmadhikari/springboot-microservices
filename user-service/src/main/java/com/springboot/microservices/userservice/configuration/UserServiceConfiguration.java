29640640e16206b891e2a6c9227bcffa57c0224f
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2023-08-24 17:53:24 +0530

    REFACTORED: Moved class to configuration package and applied annotation @LoadBalanced

--- user-service/src/main/java/com/springboot/microservices/userservice/configuration/UserServiceConfiguration.java
+++ user-service/src/main/java/com/springboot/microservices/userservice/configuration/UserServiceConfiguration.java
@@ -1,5 +1,6 @@
-package com.springboot.microservices.userservice;
+package com.springboot.microservices.userservice.configuration;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -8,6 +9,7 @@ import org.springframework.web.client.RestTemplate;
 public class UserServiceConfiguration {
 
 	@Bean
+	@LoadBalanced
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
