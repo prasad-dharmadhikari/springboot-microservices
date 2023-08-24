7896a664700f29780a28fe384952d5ece20fdc7e
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2023-08-24 16:23:34 +0530

    ADDED: Hotel entity field

--- user-service/src/main/java/com/springboot/microservices/userservice/entities/Rating.java
+++ user-service/src/main/java/com/springboot/microservices/userservice/entities/Rating.java
@@ -4,15 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class Rating {
 	private String ratingId;
 	private String userId;
 	private String hotelId;
+	private Hotel hotel;
 	private int rating;
 	private String feedBack;
 }
