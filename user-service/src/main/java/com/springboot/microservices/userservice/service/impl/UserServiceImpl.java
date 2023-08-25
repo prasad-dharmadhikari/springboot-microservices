208a321665990904f7ada8bd73d35468bcf99535
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2023-08-25 12:50:26 +0530

    REFACTORED: Code to call microservices via feign client

--- user-service/src/main/java/com/springboot/microservices/userservice/service/impl/UserServiceImpl.java
+++ user-service/src/main/java/com/springboot/microservices/userservice/service/impl/UserServiceImpl.java
@@ -1,22 +1,20 @@
 package com.springboot.microservices.userservice.service.impl;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.springboot.microservices.userservice.entities.Hotel;
 import com.springboot.microservices.userservice.entities.Rating;
 import com.springboot.microservices.userservice.entities.User;
 import com.springboot.microservices.userservice.exceptions.ResourceNotFoundException;
+import com.springboot.microservices.userservice.external.services.HotelService;
+import com.springboot.microservices.userservice.external.services.RatingService;
 import com.springboot.microservices.userservice.repository.UserRepository;
 import com.springboot.microservices.userservice.service.UserService;
 
@@ -24,11 +22,15 @@ import com.springboot.microservices.userservice.service.UserService;
 public class UserServiceImpl implements UserService {
 
 	private Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
-	@Autowired
-	private UserRepository userRepository;
 
 	@Autowired
-	private RestTemplate restTemplate;
+	private UserRepository userRepository;
+	
+	@Autowired
+	private HotelService hotelService;
+	
+	@Autowired
+	private RatingService ratingService;
 
 	@Override
 	public User saveUser(User user) {
@@ -39,8 +41,8 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public List<User> getAllUsers() {
 
-		List<User> users = userRepository.findAll();
-		return users.stream().map(user -> {
+		//List<User> users = userRepository.findAll();
+		return userRepository.findAll().stream().map(user -> {
 			user.setRatings(getRatingsAndHotelsByUserId(user.getUserId()));
 			return user;
 		}).collect(Collectors.toList());
@@ -51,9 +53,7 @@ public class UserServiceImpl implements UserService {
 		User user = userRepository.findById(userId)
 				.orElseThrow(() -> new ResourceNotFoundException("User not found with given id: " + userId));
 		// fetch rating from rating-service and hotels associated from hotel-service
-
 		user.setRatings(getRatingsAndHotelsByUserId(userId));
-
 		return user;
 	}
 
@@ -69,12 +69,9 @@ public class UserServiceImpl implements UserService {
 
 	private List<Rating> getRatingsAndHotelsByUserId(String userId) {
 
-		String ratingServiceUrl = "http://RATING-SERVICE/ratings/users?userId=" + userId;
-		String hotelServiceUrl = "http://HOTEL-SERVICE/hotels/";
-		Rating[] ratingsOfUser = restTemplate.getForObject(ratingServiceUrl, Rating[].class);
-		// List<Rating> ratings = Arrays.stream(ratingsOfUser).toList();
-		List<Rating> ratingList = Arrays.stream(ratingsOfUser).toList().stream().map(rating -> {
-			Hotel hotel = restTemplate.getForEntity(hotelServiceUrl + rating.getHotelId(), Hotel.class).getBody();
+		List<Rating> ratingsOfUser = ratingService.getRatingsByUserId(userId);
+		List<Rating> ratingList = ratingsOfUser.stream().map(rating -> {
+			Hotel hotel = hotelService.getHotelById(rating.getHotelId());
 			rating.setHotel(hotel);
 			return rating;
 		}).collect(Collectors.toList());
