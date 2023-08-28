c52f80ea7449c2828c275a11999717d221b5ee80
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2023-08-28 18:23:44 +0530

    ADDED: CircuitBraker annotation and implementation for fallback method

--- user-service/src/main/java/com/springboot/microservices/userservice/controller/UserController.java
+++ user-service/src/main/java/com/springboot/microservices/userservice/controller/UserController.java
@@ -2,6 +2,8 @@ package com.springboot.microservices.userservice.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,9 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 import com.springboot.microservices.userservice.entities.User;
 import com.springboot.microservices.userservice.service.UserService;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
+	
+	private Logger logger = LoggerFactory.getLogger(UserController.class);
 
 	@Autowired
 	private UserService userService;
@@ -28,8 +34,22 @@ public class UserController {
 	}
 	
 	@GetMapping("/{id}")
+	@CircuitBreaker(name = "ratingHotelBraker", fallbackMethod = "ratingHotelFallback")
 	public ResponseEntity<User> getUserById(@PathVariable("id") String userId) {
 		return new ResponseEntity<>(userService.getUserByID(userId),HttpStatus.OK);
+	}
+	
+	//create ratingFallback method for circuit braker
+	
+	public ResponseEntity<User> ratingHotelFallback(String id, Exception ex) {
+		logger.info("Fall back is executed because service is down: ",ex.getMessage());
+		User user = User.builder()
+			.email("dummy@gmail.com")
+			.name("Dummy Name")
+			.about("This is user is created dummy because some service is down")
+			.userId(id)
+			.build();
+		return new ResponseEntity<>(user, HttpStatus.OK);
 	}
 	
 	@GetMapping
