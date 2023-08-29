3cb03a4e364bf4fcf61adb0ac746fd3839d0ecc5
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2023-08-29 18:05:22 +0530

    ADDED: Code,mapping auth response and spring security related configuration to enable okta auth

---
+++ api-gateway/src/main/java/com/springboot/microservices/apigateway/models/AuthResponse.java
@@ -0,0 +1,27 @@
+package com.springboot.microservices.apigateway.models;
+
+import java.util.Collection;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AuthResponse {
+
+	private String userId;
+
+	private String accessToken;
+	
+	private String refreshToken;
+	
+	private long expireAt;
+	
+	private Collection<String> authorities;
+}
